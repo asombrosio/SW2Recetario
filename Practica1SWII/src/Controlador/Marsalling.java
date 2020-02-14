594ad88a549ae2fe15a32b1774dcd55b814c3681
David Recio Arnés <davidrecioarnes@gmail.com>
2020-02-14 10:57:11 +0100

    modificando estructura y marsalling

--- Practica1SWII/src/Controlador/Marsalling.java
+++ Practica1SWII/src/Controlador/Marsalling.java
@@ -5,10 +5,46 @@
  */
 package Controlador;
 
+import Recetarios.Receta;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+import javax.xml.bind.PropertyException;
+import javax.xml.bind.Unmarshaller;
+
 /**
  *
  * @author david
  */
 public class Marsalling {
-    
+Receta receta= new Receta();
+
+public void crearXML(String nombreFichero){
+        FileWriter fichero = null;
+
+        try {
+            fichero = new FileWriter("./files/" + nombreFichero);
+
+            try {
+                // TODO code application logic here
+                JAXBContext context = JAXBContext.newInstance(Receta.class);
+                Marshaller m = context.createMarshaller();
+                m.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+                m.marshal(receta, fichero);
+
+            } catch (PropertyException ex) {
+                Logger.getLogger(Marsalling.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (JAXBException ex) {
+                Logger.getLogger(Marsalling.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(Marsalling.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
+        
