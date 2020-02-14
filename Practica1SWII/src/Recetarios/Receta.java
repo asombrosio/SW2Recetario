0ee4b0385dfd31a4a71c3b1b12c392152e3c2520
asombrosio <asombrosio89@gmail.com>
2020-02-14 10:41:52 +0100

    Comienzo

---
+++ Practica1SWII/src/Recetarios/Receta.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Recetarios;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author darth
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlRootElement(name = "Receta")
+public class Receta implements Serializable {
+    @XmlTransient
+    private Integer codigo;
+    @XmlElement
+   private String nombre;
+   @XmlElementWrapper 
+   private ArrayList<String> ingredienete = new ArrayList<>();
+   @XmlElement
+   private Double precio;
+    
+    public Receta (){}
+
+    /**
+     * @return the codigo
+     */
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the ingredienete
+     */
+    public ArrayList<String> getIngredienete() {
+        return ingredienete;
+    }
+
+    /**
+     * @param ingredienete the ingredienete to set
+     */
+    public void setIngredienete(ArrayList<String> ingredienete) {
+        this.ingredienete = ingredienete;
+    }
+
+    /**
+     * @return the precio
+     */
+    public Double getPrecio() {
+        return precio;
+    }
+
+    /**
+     * @param precio the precio to set
+     */
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+
+ 
+}
---
+++ Practica1SWII/src/Recetarios/Recetario.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Recetarios;
+
+/**
+ *
+ * @author darth
+ */
+public class Recetario {
+    private Integer codigo;
+    private String nombre;
+    private Integer cantidad;
+    private Double precio;
+    
+    public Recetario (){}
+}
