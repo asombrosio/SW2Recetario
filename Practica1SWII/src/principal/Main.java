0f9b1574763e4d74293b6da578f03c3c1ccc2f94
asombrosio <asombrosio89@gmail.com>
2020-02-14 11:16:42 +0100

    Menu empezado

--- Practica1SWII/src/principal/Main.java
+++ Practica1SWII/src/principal/Main.java
@@ -5,6 +5,9 @@
  */
 package principal;
 
+import java.util.Scanner;
+
+
 /**
  *
  * @author darth
@@ -14,9 +17,54 @@ public class Main {
     /**
      * @param args the command line arguments
      */
+    
+    
     public static void main(String[] args) {
         // TODO code application logic here
+    Scanner scanner = new Scanner(System.in);
+    Integer opcion = -1;
+    Integer salir = 0;
         
+        while(opcion != salir){
+            System.err.println("Elige una opcion, pulsa 0 para salir");
+            opcion = Integer.parseInt(scanner.nextLine());
+            switch(opcion){
+                case 1:
+                    //Exportar agenda
+                    break;
+                case 2:
+                    // Importar agenda
+                    break;
+                case 3:
+                    // Exportar Persona agenda
+                    break;
+                case 4:
+                    // Importar Persona agenda
+                    break;
+                case 5:
+                    // Validacion DTD de la agenda
+                    break;
+                case 6:
+                    // Validacion contra XSD Agenda
+                    break;
+                case 7:
+                    // Escribir sentencia Xpath y ejecutarla
+                    break;
+                case 8:
+                    // Consulat xQuery a la Agenda
+                    break;
+                case 9:
+                    // Añadir persona
+                    break;
+                case 10:
+                    // Listar Agenda
+                    break;
+                default: 
+                    System.err.println("Error, introduzca un numero del cero al 10");
+                    break;
+            }
+        }
     }
     
 }
+
