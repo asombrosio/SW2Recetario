ca5b1af0a53d56dff5bcb40d42c65e01202f2d0d
asombrosio <asombrosio89@gmail.com>
2020-02-20 12:47:00 +0100

    Xpath empezado

---
+++ Practica1SWII/src/Controlador/Xpath.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+/**
+ *
+ * @author darth
+ */
+import java.io.File;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
+public class Xpath {
+    
+}
