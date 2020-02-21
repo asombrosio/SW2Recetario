d6461a781b49c593f882645d4cede25b5449fc26
asombrosio <asombrosio89@gmail.com>
2020-02-21 11:43:46 +0100

    Xpath con ERRORES

--- Practica1SWII/src/Controlador/Xpath.java
+++ Practica1SWII/src/Controlador/Xpath.java
@@ -1,42 +1,63 @@
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package Controlador;
-//
-///**
-// *
-// * @author darth
-// */
-//import java.io.File;
-//
-//import javax.xml.parsers.DocumentBuilder;
-//import javax.xml.parsers.DocumentBuilderFactory;
-//import javax.xml.xpath.XPath;
-//import javax.xml.xpath.XPathConstants;
-//import javax.xml.xpath.XPathFactory;
-//
-//import org.w3c.dom.Document;
-//import org.w3c.dom.NodeList;
-//
-//public class Xpath {
-//    // La expresion xpath de busqueda
-//    String xPathExpression = "//satelite[@nombre='Luna']";
-//		
-//    // Carga del documento xml
-//    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-//    DocumentBuilder builder = factory.newDocumentBuilder();
-//    Document documento = builder.parse(new File("prueba.xml"));
-//
-//    // Preparación de xpath
-//    XPath xpath = XPathFactory.newInstance().newXPath();
-//
-//    // Consultas
-//    NodeList nodos = (NodeList) xpath.evaluate(xPathExpression, documento, XPathConstants.NODESET);
-//
-//    for (int i=0;i<nodos.getLength();i++){
-//	System.out.println(nodos.item(i).getNodeName()+" : " +
-//        nodos.item(i).getAttributes().getNamedItem("nombre"));
-//		}
-//}
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controlador;
+
+/**
+ *
+ * @author darth
+ */
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class Xpath {
+    
+    File xmlFile = new File("Recetario.xml");
+    
+    //Mostrar todos los receta
+    String expressionXPath = "//receta";
+    
+     try {
+            //Carga del documento xml
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = factory.newDocumentBuilder();
+            Document documento = builder.parse(xmlFile);
+            
+            //preparacion de xpath
+            XPath xpath = XPathFactory.newInstance().newXPath();
+            
+            //consultas
+            NodeList nodos = (NodeList) xpath.evaluate(expressionXPath,documento,XPathConstants.NODESET);
+            //Imprime solo categoria de libros resultado
+            for (int i = 0; i < nodos.getLength(); i++) {
+                System.out.println(nodos.item(i).getNodeName() + " : " +
+                        + nodos.item(i)).getAttributes().getNamedItem("nombre"));
+
+            }
+
+        } catch (SAXException ex) {
+            Logger.getLogger(Xpath.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Xpath.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (XPathExpressionException ex) {
+            Logger.getLogger(Xpath.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ParserConfigurationException ex) {
+            Logger.getLogger(Xpath.class.getName()).log(Level.SEVERE, null, ex);
+        }
+}
